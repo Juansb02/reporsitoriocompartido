630f6e5f5684953a68d5eaa3076d1c2c1044f496
SONIA MILENA <soniamilenam@gmail.com>
2021-10-04 23:57:35 +0200

    se crean carpetas de views

---
+++ Vesrsion 1/Panasea Vue/panasea/views/Categorias.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="container" id="parrafo">
+    <div class="container div-galeria" id="galeria">
+        <div class="row fila-productos">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+            </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 tarjeta-menu">
+                    <a href="suplementos.html"><img href="suplementos.html" class="imagen-menu" src="imagenes\suplementosMenu.jpg"></a>
+                <h4 align="center" class ="nombre-categoria">Suplementos</h4>
+                </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 tarjeta-menu">
+            
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 tarjeta-menu">
+                <a href="CuidadoPersonal.html"><img class="imagen-menu" src="imagenes\cuidadoPersonalMenu.jpg"></a>
+                <h4 align="center" class ="nombre-categoria">Cuidado Personal</h4>
+            </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+        </div>
+
+        <div class="container div-galeria" id="galeria">
+        <div class="row fila-productos">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+
+        <div class="row fila-productos">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 tarjeta-menu">
+            <a href="lineavegana.html"><img class="imagen-menu" src="imagenes\lieaVeganaMenu.jpg"></a>
+                <h4 align="center" class ="nombre-categoria">Línea Vegana</h4>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 tarjeta-menu"></div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 tarjeta-menu">
+                <a href="Alimentos.html"><img class="imagen-menu" src="imagenes\alimentosMenu.jpg"></a>
+                <h4 align="center" class ="nombre-categoria">Alimentos</h4>            
+            </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+        </div>
+    </div>
+    </div>
+</div>
+    </template>
