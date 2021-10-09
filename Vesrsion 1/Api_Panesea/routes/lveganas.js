95c1a1935bb5ba41b9a7c5764681b95b641b702b
bernaldaniel743 <bernaldaniel743@gmail.com>
2021-10-08 19:52:49 -0500

    Se crea Api de Panasea y conexión con la base de datos

---
+++ Vesrsion 1/Api_Panesea/routes/lveganas.js
@@ -0,0 +1,25 @@
+// Exportar configuración
+module.exports = router;
+
+import express from 'express';
+const router = express.Router();
+
+// Importar modelo
+import lveganas from '../models/lveganas';
+
+// Ruta para consultar todos los productos
+router.get('/lveganas', async(req, res) => {
+    try {
+        const lveganasDB = await lveganas.find();
+        res.json(lveganasDB);
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Se presentó un error al consultar los lveganas',
+      
+ error
+        })
+    }
+});
+
+// Exportar configuración
+module.exports = router;
